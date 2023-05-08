13d41b6b55c015cc4bd2c73d569c25be3be7d01f
Omar Baltaji <omar.baltaji@app4legal.com>
2023-05-09 01:14:42 +0300

    created list service to handle the fetching of all lists, and creating, deleting, and updating a list

---
+++ src/list/list.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { CreateListDto, EditListDto } from './dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { List } from './list.schema';
+
+@Injectable()
+export class ListService {
+  constructor(@InjectModel(List.name) private listModel: Model<List>) {}
+
+  async getLists() {
+    const lists = await this.listModel.find().populate('items');
+    return {
+      count: lists.length,
+      lists,
+    };
+  }
+
+  async createList(dto: CreateListDto): Promise<any> {
+    return {
+      list: await this.listModel.create({ title: dto.title, items: [] }),
+    };
+  }
+
+  async deleteList(id: string): Promise<string> {
+    const list = await this.listModel.findOneAndRemove({ _id: id });
+
+    if (!list) return `List with id ${id} does not exist`;
+
+    // delete all items related to list
+    // await ItemModel.deleteMany({ list: listId }).exec();
+
+    return `List "${list.title}" deleted successfully`;
+  }
+
+  async updateList(id: string, dto: EditListDto) {
+    const list = await this.listModel
+      .findByIdAndUpdate(
+        { _id: id },
+        { title: dto.title },
+        { runValidators: true, new: true },
+      )
+      .populate('items');
+
+    if (!list) return `List with id ${id} does not exist`;
+
+    return { list };
+  }
+}
