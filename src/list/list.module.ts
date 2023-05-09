8179605e5c97b49fc9284fa17ee21f3bc72f9683
Omar Baltaji <omar.baltaji@app4legal.com>
2023-05-10 00:04:45 +0300

    imported item model to be used in the list service

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -3,10 +3,12 @@ import { ListController } from './list.controller';
 import { ListService } from './list.service';
 import { List, ListSchema } from './list.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Item, ItemSchema } from 'src/item/item.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: List.name, schema: ListSchema }]),
+    MongooseModule.forFeature([{ name: Item.name, schema: ItemSchema }]),
   ],
   controllers: [ListController],
   providers: [ListService],
