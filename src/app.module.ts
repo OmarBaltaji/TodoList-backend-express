82007503f8d3eb79f83b149168993b6138598074
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-14 23:14:52 +0300

    Importing graphql in app module and momentarily disabling custom filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ItemModule } from './item/item.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { APP_FILTER } from '@nestjs/core';
-import { CustomExceptionFilter } from './exception-filter/custom-exception.filter';
+// import { CustomExceptionFilter } from './exception-filter/custom-exception.filter';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -12,12 +14,17 @@ import { CustomExceptionFilter } from './exception-filter/custom-exception.filte
     ListModule,
     ItemModule,
     MongooseModule.forRoot(process.env.ATLAS_URI),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
   ],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: CustomExceptionFilter,
-    },
-  ],
+  // providers: [
+  //   {
+  //     provide: APP_FILTER,
+  //     useClass: CustomExceptionFilter,
+  //   },
+  // ],
 })
 export class AppModule {}
