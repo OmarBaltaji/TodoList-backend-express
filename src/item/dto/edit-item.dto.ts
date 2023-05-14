269760846711aaa139d20a831b58535d3959687d
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-14 23:21:03 +0300

    Adding Input Types for item

--- src/item/dto/edit-item.dto.ts
+++ src/item/dto/edit-item.dto.ts
@@ -1,15 +1,20 @@
 /* eslint-disable prettier/prettier */
+import { Field, InputType } from '@nestjs/graphql';
 import { IsString, IsBoolean, IsOptional } from 'class-validator';
 
+@InputType()
 export class EditItemDto {
+  @Field({ nullable: true })
   @IsString()
   @IsOptional()
   name?: string;
 
+  @Field({ nullable: true })
   @IsBoolean()
   @IsOptional()
   done?: boolean;
 
+  @Field({ nullable: true })
   @IsString()
   @IsOptional()
   listId?: string;
