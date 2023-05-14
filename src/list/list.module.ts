0ac145bb66357990951a5d615820d69fa570ec64
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-14 23:16:54 +0300

    Adding list resolver to list module

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -4,6 +4,7 @@ import { ListService } from './list.service';
 import { List, ListSchema } from './list.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Item, ItemSchema } from 'src/item/item.schema';
+import { ListResolver } from './list.resolver';
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { Item, ItemSchema } from 'src/item/item.schema';
     MongooseModule.forFeature([{ name: Item.name, schema: ItemSchema }]),
   ],
   controllers: [ListController],
-  providers: [ListService],
+  providers: [ListService, ListResolver],
   exports: [
     MongooseModule.forFeature([{ name: List.name, schema: ListSchema }]),
   ],
