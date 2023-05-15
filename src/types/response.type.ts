1709354bd939191eae02323f97bd7a13ee804198
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-16 00:47:49 +0300

    Moved list response to its own file and placed it under the newly created list types directory

--- src/types/response.type.ts
+++ src/types/response.type.ts
@@ -1,14 +1,4 @@
 import { Item } from 'src/item/item.schema';
-import { List } from 'src/list/list.schema';
-
-export interface ListResponse {
-  list: List;
-}
-
-export interface ListsResponse {
-  count: number;
-  lists: List[];
-}
 
 export interface ItemResponse {
   item: Item;
