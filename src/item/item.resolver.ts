1e9f34ebc78b877f29900de5920febc2291b0682
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-16 01:06:55 +0300

    Modified response type in graphql and removed the typescript response types

--- src/item/item.resolver.ts
+++ src/item/item.resolver.ts
@@ -1,26 +1,29 @@
 import { Mutation, Resolver, Args } from '@nestjs/graphql';
 import { ItemService } from './item.service';
-import { CreateItemDto, EditItemDto } from './dto';
-import { Item } from './item.schema';
-import { ItemType } from './item.type';
+import {
+  CreateItemDto,
+  EditItemDto,
+  ItemType,
+  ItemResponseType,
+} from './types';
 
 @Resolver((of) => ItemType)
 export class ItemResolver {
   constructor(private itemService: ItemService) {}
 
-  @Mutation((returns) => ItemType)
-  createItem(@Args('dto') dto: CreateItemDto): Promise<Item> {
+  @Mutation((returns) => ItemResponseType)
+  createItem(@Args('dto') dto: CreateItemDto) {
     return this.itemService.createItem(dto);
   }
 
   @Mutation((returns) => String)
-  deleteItem(@Args('id') id: string): Promise<any> {
+  deleteItem(@Args('id') id: string) {
     return this.itemService.deleteItem(id);
   }
 
-  @Mutation((returns) => ItemType)
+  @Mutation((returns) => ItemResponseType)
   // eslint-disable-next-line prettier/prettier
-  updateItem(@Args('id') id: string, @Args('dto') dto: EditItemDto): Promise<Item | string> {
+  updateItem(@Args('id') id: string, @Args('dto') dto: EditItemDto){
     return this.itemService.updateItem(id, dto);
   }
 }
