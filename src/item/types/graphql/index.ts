1018de40f6aad95f76ecbaf0364b9b6cf887b648
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-16 01:04:36 +0300

    created new graphql folder in the types folder for item

---
+++ src/item/types/graphql/index.ts
@@ -0,0 +1,2 @@
+export * from './item-response.type';
+export * from './item.type';
---
+++ src/item/types/graphql/item.type.ts
@@ -0,0 +1,17 @@
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { ListType } from 'src/list/types/graphql/list.type';
+
+@ObjectType('Item')
+export class ItemType {
+  @Field((type) => ID)
+  _id: string;
+
+  @Field()
+  name: string;
+
+  @Field({ defaultValue: false })
+  done?: boolean;
+
+  @Field((type) => ListType)
+  list?: string;
+}
