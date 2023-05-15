6331b7bc7c171d5015baa134c458affa6454848f
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-16 01:06:19 +0300

    disabled the item controller temporarily

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,28 +1,28 @@
 import { Body, Controller, Delete, Param, Patch, Post } from '@nestjs/common';
 import { ItemService } from './item.service';
-import { CreateItemDto, EditItemDto } from './dto';
-import { Item } from './item.schema';
+import { CreateItemDto, EditItemDto } from './types/dto';
+import { Item } from './types/item.schema';
 import { TransformObjectId } from 'src/decorators';
 
 @Controller('api/v1/items')
 export class ItemController {
-  constructor(private item: ItemService) {}
+  // constructor(private item: ItemService) {}
 
-  @Post('add')
-  createItem(
-    @Body(new TransformObjectId('listId')) dto: CreateItemDto,
-  ): Promise<Item> {
-    return this.item.createItem(dto);
-  }
+  // @Post('add')
+  // createItem(
+  //   @Body(new TransformObjectId('listId')) dto: CreateItemDto,
+  // ): Promise<Item> {
+  //   return this.item.createItem(dto);
+  // }
 
-  @Delete(':id')
-  deleteItem(@Param('id') id: string): Promise<any> {
-    return this.item.deleteItem(id);
-  }
+  // @Delete(':id')
+  // deleteItem(@Param('id') id: string): Promise<any> {
+  //   return this.item.deleteItem(id);
+  // }
 
-  @Patch(':id')
-  // eslint-disable-next-line prettier/prettier
-  updateItem(@Param('id') id: string, @Body() dto: EditItemDto): Promise<Item | string> {
-    return this.item.updateItem(id, dto);
-  }
+  // @Patch(':id')
+  // // eslint-disable-next-line prettier/prettier
+  // updateItem(@Param('id') id: string, @Body() dto: EditItemDto): Promise<Item | string> {
+  //   return this.item.updateItem(id, dto);
+  // }
 }
