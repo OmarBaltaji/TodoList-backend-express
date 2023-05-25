490999f2b07c65c5c693c0aa71e46e0d1819a9f8
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-25 12:27:25 +0300

    Created auth module which is responsible for authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { JwtStrategy } from './strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/user/user.schema';
+import { AuthResolver } from './auth.resolver';
+
+@Module({
+  imports: [
+    JwtModule.register({}),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [JwtStrategy, AuthService, AuthResolver],
+  controllers: [AuthController],
+})
+export class AuthModule {}
