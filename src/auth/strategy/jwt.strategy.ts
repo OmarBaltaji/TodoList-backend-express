8c84b95a73215bcecc108a5f0621b2b17c726737
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-25 12:29:39 +0300

    Created JWT strategy and guard to protect certain routes from being accessed by unauthorized users

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { PassportStrategy } from '@nestjs/passport';
+import { Model } from 'mongoose';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/user/user.schema';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userModel.findOne({ _id: payload.sub });
+    delete user.password;
+    return user;
+  }
+}
