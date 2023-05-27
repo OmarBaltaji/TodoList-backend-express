214978c0eb696d0b204286d103cbb5828044418d
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-27 16:56:59 +0300

    Setting the access token from the server as soon as the user logs in

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,20 +1,29 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { LoginAuthDto, RegisterAuthDto } from './dto';
-import { UserType } from 'src/user/types';
-import { UserResponseType } from 'src/user/types';
+import { AuthResponseType, UserType } from 'src/user/types';
+import { ResponseCookie } from 'src/decorators/response-cookie.decorator';
+import { Response } from 'express';
 
 @Resolver((type) => UserType)
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
-  @Mutation((returns) => UserResponseType)
+  @Mutation((returns) => AuthResponseType)
   register(@Args('dto') dto: RegisterAuthDto) {
     return this.authService.register(dto);
   }
 
-  @Mutation((returns) => UserResponseType)
-  login(@Args('dto') dto: LoginAuthDto) {
-    return this.authService.login(dto);
+  @Mutation((returns) => AuthResponseType)
+  async login(@Args('dto') dto: LoginAuthDto, @ResponseCookie() res: Response) {
+    const { access_token } = await this.authService.login(dto);
+    res.cookie('access_token', access_token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    });
+
+    return { result: true };
   }
 }
