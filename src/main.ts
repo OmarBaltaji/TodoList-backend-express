abd0f974e7a9b7233ff2fcb31f9e6032ba57928c
Omar Baltaji <omr.baltaji@gmail.com>
2023-05-29 01:22:04 +0300

    Added env-cmd library and used different origins for dev and prod environments

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.URI_ORIGIN,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
