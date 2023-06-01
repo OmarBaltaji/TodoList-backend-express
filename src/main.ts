892da99381da31014d9707d273ec5483d8084a60
Omar Baltaji <omr.baltaji@gmail.com>
2023-06-02 00:25:42 +0300

    remove console logging the URI_ORIGIN

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.URI_ORIGIN);
   app.enableCors({
     origin: process.env.URI_ORIGIN,
     credentials: true,
