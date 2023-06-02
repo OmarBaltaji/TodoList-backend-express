ac1319b4e663536d796680eb03dea04706c7e23d
Omar Baltaji <omr.baltaji@gmail.com>
2023-06-03 01:15:07 +0300

    Adding allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.URI_ORIGIN,
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
+  console.log(process.env.PORT);
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
